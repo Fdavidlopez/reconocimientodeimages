15d212047e6d3f0dcb80dfea76cc7306e5d84c74
fdavidlopez <fdlopez312@ieszaidinvergeles.org>
2022-01-27 10:39:32 +0000

    Proyecto Reconocimiento Facil

--- ejemplo2.php
+++ ejemplo2.php
@@ -17,14 +17,16 @@ if (isset($_GET['file']) && isset($_GET['name'])){
     <meta charset="UTF-8">
     <title>Face Detector</title>
     <link rel="stylesheet" href="https://unpkg.com/jcrop/dist/jcrop.css">
+    <link rel="stylesheet" href="estilos/estilos2.css">
     <script src="https://unpkg.com/jcrop"></script>
 </head>
 <body>
-    <img src="<?= $file  ?> " alt="imagen subida" id="imagen">
+    <img src="<?= $file  ?> " alt="imagen subida" id="imagen" class="imagenescaneada">
     <form action="ejemplo3.php" method="post" id="fblur">
+        <input type="submit" value="Procesar filtrado de edad !!" class="boton"/>
         <input type="hidden" name="file" value="<?= $file ?>"/>
         <input type="hidden" name="name" value="<?= $name ?>"/>
-        <input type="submit" value="Procesar filtrado de edad !!"/>
+        
     </form>
     <script src= "service.js"></script>
 </body>
